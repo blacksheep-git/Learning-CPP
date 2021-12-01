be1399d83df8cfa4e148f9d53e4feeacdafcaeff
teo <m@teoo.io>
2021-12-01 11:37:56 -0800

    Runs

--- Account.cpp
+++ Account.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 inline ostream& operator<<(ostream& os, const Account& acct) {
-    os << acct.number << ':' << acct.customer << ':' << acct.balance;
+    os << acct.number << " : " << acct.customer <<  " : " << acct.balance;
     return os;
 }
 
